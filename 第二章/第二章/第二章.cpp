64b7a51636bfea1ddb741567113ad13bc4f5eda5
ytl1427698367 <1427698367@qq.com>
2018-10-09 23:03:46 +0800

    提交

--- 第二章/第二章/第二章.cpp
+++ 第二章/第二章/第二章.cpp
@@ -22,23 +22,56 @@
 //	system("pause");
 //	return 0;
 //}
-void simon(int);
-int main(){
-	using namespace std;
-	simon (3);	
-	cout << "pick an integer;";
-	int count;
-	cin >> count;
-	simon(count);
-	cout << "done\n";
-	return 0;
-}
-		void simon(int n)	
-		{
-			using namespace std;
-			cout << "simon saystouch your toes" << n << "times.\n";
+//void simon(int);
+//int main(){
+//	using namespace std;
+//	simon (3);	
+//	cout << "pick an integer;";
+//	int count;
+//	cin >> count;
+//	simon(count);
+//	cout << "done\n";
+//	return 0;
+//}
+//		void simon(int n)	
+//		{
+//			using namespace std;
+//			cout << "simon saystouch your toes" << n << "times.\n";
+//
+//			system("pause");
+//		}
+//int main(){
+//	using namespace std;
+//	char name;
+//	char address;
+//	cout << "请输入你的名字：\n";
+//     cin >> name;
+//     cout << "你的名字是" << name;
+//	cout << "你家地址是\n";
+//	cin >> address;
+//	
+//	 cout<<"你的地址是" << address;
+//		
+//	system("pause");
+//	return 0;
+//
+//
+//}
 
-			system("pause");
-		}
+	using namespace std;
+	double feetTof(double furlongs);
+	int main(){
+		cout << "enter a distance in furlongs:";
+		double furlongs;
+		cin >> furlongs;
+		double feet;
+		feet = feetTof(furlongs);
+		cout << furlongs << "furlongs=" << feet << " feet.\n";
+	
+	}
+	double feetTof(double){
+		return 220;
+
+}
 
 
