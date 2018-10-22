44e5fe57ee4e5ac452783dc0722e6998e92d5aa1
ytl1427698367 <1427698367@qq.com>
2018-10-22 22:43:44 +0800

    提交

--- 练习/练习/练习.c
+++ 练习/练习/练习.c
@@ -1,12 +1,7 @@
-//查找一个字符数组的长度
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<CoreWindow.h>
-#include<string.h>
 #include<time.h>
-#include<math.h>
-#include<conio.h>
 //int Strlen(char*str)
 //{
 //	int count = 0;
@@ -139,7 +134,6 @@ int main()
 //
 //	}
 //}
-
 //求第n项斐波那契数列,递归法
 //int fib(int n)
 //{
@@ -155,7 +149,6 @@ int main()
 //	system("pause");
 //	return 0;
 //}
-
 //非递归法
 //int fib(int n){
 //	if (n == 1 || n == 2)
@@ -225,164 +218,164 @@ int main()
 //	return 0;
 //
 //}
-//int  DigitSum(int n)
-//{
-//	int   m=0;
-//	if (n == 0){
-//		return 0;
-//	}
-//	 
-//		m = n % 10;
-//		return  m + DigitSum(n/10);
-//}
+////int  DigitSum(int n)
+////{
+////	int   m=0;
+////	if (n == 0){
+////		return 0;
+////	}
+////	 
+////		m = n % 10;
+////		return  m + DigitSum(n/10);
+////}
+////	
+////
+////int main()
+////{
+////    printf("%d\n", DigitSum(1168));
+////	system("pause");
+////	return 0;
+////}
+////void reverse_string(char*str)
+////{
+////	if (*str == '\0')
+////	{
+////		printf("%c", *str);
+////	}
+////	else
+////	{
+////		reverse_string(++str);
+////		printf("%c", *(--str));
+////		system("pause");
+////	}
+////}
+////int main()
+////{
+////	char st[] = "hfgfkiotyrvg";
+////	reverse_string(st);
+////	return 0;
+////}
+////int memu(){
+////	printf("**********************************\n");
+////	printf(" 开始游戏  结束游戏\n");
+////	printf("**************************\n");
+////	printf("请输入你的选择:\n");
+////	int choice = 0;
+////	scanf("%d", &choice);
+////	return choice;
+////}
+////void game(){
+////	int result = rand()%100 + 1;
+////	while (1)
+////	{
+////		printf("请输入一个数字：");
+////		int num = 0;
+////		scanf("%d", &num);
+////		if (num < result)
+////		{
+////			printf("低了\n");
+////		}
+////		else if (num>result)
+////		{
+////			printf("高了\n");
+////		}
+////		else
+////		{
+////			printf("恭喜你，答对了\n");
+////
+////			break;
+////		}
+////	}
+////}
+////int main()
+////{
+////	srand(time(NULL));
+////	while (1)
+////	{
+////		int choice = memu();
+////		if (choice == 1)
+////		{
+////			game();
+////		}
+////		else if (choice == 0)
+////		{
+////			break;
+////		}
+////		else
+////		{
+////			printf("你输入的数字有误，请重新输入\n");
+////		}
+////	}
+////	system("pause");
+////	return 0;
+////
+////}
+////         //实现一个数组的排序问题
+////void Swape(int*x, int*y)
+////{
+////	int temp = *x;
+////	*x = *y;
+////	*y = temp;
+////}
+////void bobblesort(int arr[], int size)
+////{
+////	int board = 0;          //定义一个边界
+////	for (; board < size; ++board)
+////	{
+////		int cur = size - 1;
+////		for (; cur>board; cur--)
+////		{
+////			if (arr[cur]<arr[cur-1])
+////			{
+////				Swape(&arr[cur], &arr[cur - 1]);
+////			}
+////		}	
+////				
+////	}
+////
+////}
+////int main()
+////{
+////	int arr[] = { 9, 5, 6, 3, 7, 56, 2 };
+////	int lep = sizeof(arr) / sizeof(arr[0]);
+////	bobblesort(arr, lep);
+////	for (int i = 0; i < lep; i++)
+////	{
+////		printf("%d ", arr[i]);
+////
+////	}
+////	system("pause");
+////	return 0;
+////}
+//
+////比较两个整数二进制中，有多少个位不同；
+////int two_bits(int n, int m)
+////{
+////	int count = 0;
+////	int x = n^m;
+////	while (x != 0)
+////	{
+////		if ((x & 1)== 1)
+////		{
+////		count++;
+////		}
+////		x=x >>1;
+////	}
+////		return count;
+////	
+////}
+////int main()
+////{
+////	printf("请输入两个数num1和num2");
+////	int num1 = 0, num2 = 0;
+////	scanf("%d %d", &num1,&num2);
+////		printf("%d %d", two_bits(num1, num2));
+////		system("pause");
+////				return 0;
+////}
 //	
+////写一个函数返回参数二进制中 1 的个数
 //
-//int main()
-//{
-//    printf("%d\n", DigitSum(1168));
-//	system("pause");
-//	return 0;
-//}
-//void reverse_string(char*str)
-//{
-//	if (*str == '\0')
-//	{
-//		printf("%c", *str);
-//	}
-//	else
-//	{
-//		reverse_string(++str);
-//		printf("%c", *(--str));
-//		system("pause");
-//	}
-//}
-//int main()
-//{
-//	char st[] = "hfgfkiotyrvg";
-//	reverse_string(st);
-//	return 0;
-//}
-//int memu(){
-//	printf("**********************************\n");
-//	printf(" 开始游戏  结束游戏\n");
-//	printf("**************************\n");
-//	printf("请输入你的选择:\n");
-//	int choice = 0;
-//	scanf("%d", &choice);
-//	return choice;
-//}
-//void game(){
-//	int result = rand()%100 + 1;
-//	while (1)
-//	{
-//		printf("请输入一个数字：");
-//		int num = 0;
-//		scanf("%d", &num);
-//		if (num < result)
-//		{
-//			printf("低了\n");
-//		}
-//		else if (num>result)
-//		{
-//			printf("高了\n");
-//		}
-//		else
-//		{
-//			printf("恭喜你，答对了\n");
-//
-//			break;
-//		}
-//	}
-//}
-//int main()
-//{
-//	srand(time(NULL));
-//	while (1)
-//	{
-//		int choice = memu();
-//		if (choice == 1)
-//		{
-//			game();
-//		}
-//		else if (choice == 0)
-//		{
-//			break;
-//		}
-//		else
-//		{
-//			printf("你输入的数字有误，请重新输入\n");
-//		}
-//	}
-//	system("pause");
-//	return 0;
-//
-//}
-//         //实现一个数组的排序问题
-//void Swape(int*x, int*y)
-//{
-//	int temp = *x;
-//	*x = *y;
-//	*y = temp;
-//}
-//void bobblesort(int arr[], int size)
-//{
-//	int board = 0;          //定义一个边界
-//	for (; board < size; ++board)
-//	{
-//		int cur = size - 1;
-//		for (; cur>board; cur--)
-//		{
-//			if (arr[cur]<arr[cur-1])
-//			{
-//				Swape(&arr[cur], &arr[cur - 1]);
-//			}
-//		}	
-//				
-//	}
-//
-//}
-//int main()
-//{
-//	int arr[] = { 9, 5, 6, 3, 7, 56, 2 };
-//	int lep = sizeof(arr) / sizeof(arr[0]);
-//	bobblesort(arr, lep);
-//	for (int i = 0; i < lep; i++)
-//	{
-//		printf("%d ", arr[i]);
-//
-//	}
-//	system("pause");
-//	return 0;
-//}
-
-//比较两个整数二进制中，有多少个位不同；
-//int two_bits(int n, int m)
-//{
-//	int count = 0;
-//	int x = n^m;
-//	while (x != 0)
-//	{
-//		if ((x & 1)== 1)
-//		{
-//		count++;
-//		}
-//		x=x >>1;
-//	}
-//		return count;
-//	
-//}
-//int main()
-//{
-//	printf("请输入两个数num1和num2");
-//	int num1 = 0, num2 = 0;
-//	scanf("%d %d", &num1,&num2);
-//		printf("%d %d", two_bits(num1, num2));
-//		system("pause");
-//				return 0;
-//}
-	
-//写一个函数返回参数二进制中 1 的个数
-
 //int count_one_bits(unsigned int value)
 //{
 //	int count = 0;
@@ -401,9 +394,9 @@ int main()
 //	printf("%d", count_one_bits(15));
 //	system("pause");
 //}
-//
-//获取一个数二进制序列中所有的偶数位和奇数位，分别输出二进制序列。
-//
+////
+////获取一个数二进制序列中所有的偶数位和奇数位，分别输出二进制序列。
+////
 //int two_system(int num)
 //{
 //	if (num >1)
@@ -422,30 +415,30 @@ int main()
 //	printf("%d", two_system(15));
 //	system("pause");
 //}
-// 输出一个整数的每一位。
+//// 输出一个整数的每一位。
 //int count(int n)
 //{
-//	if (n <= 9)
+//	if (n <= 1)
 //	{
 //		printf("%d", n);
 //	}
 //	else
 //	{
-//		count(n / 10);
-//		printf("%d", n % 10);
+//		count(n / 2);
+//		printf("%d", n % 2);
 //      
 //	}
 //	}
 //int main()
 //{
-//	int num=0;
+//	int num = 0;
 //	printf("请输入一个数");
 //	scanf("%d", &num);
 //	count(num);
 //	system("pause");
 //	return 0;
 //}
-
+//
 //int main()
 //{
 //	int num = 0, i = 0;
@@ -464,7 +457,7 @@ int main()
 //	printf("\n");
 //	system("pause");
 //	return 0;
-////}
+//}
 //#define ROW 5
 //#define COL 5
 //
@@ -648,111 +641,263 @@ int main()
 //		system("pause");
 //	}
 //
-
-
-void er(int n)
-{
-	if (n < 2){
-		printf("%d", n);
-		return ;
-
-	}er(n / 2);
-	  
-		
-  
-		printf("%d", n % 2);
-	
-}
-int main()
-{
-	int num;
-	printf("请输入一个数字");
-	scanf("%d", &num);
-	 er(num);
-	 system("pause");
-	return 0;
-
-}
-//定义地图
-//#define mine_count 10
-//#define  ROW 10
-//#define  COL 10
-//int menu(){
-//	printf("  1开始游戏 \n ");
-//	printf("0结束游戏");
-//	printf("请输入你的选择");
+//void er(int n)
+//{
+//	if (n < 2){
+//		printf("%d", n);
+//		return ;
+//
+//	}er(n / 2);
+//	  
+//		
+//  
+//		printf("%d", n % 2);
+//	
+//}
+//int main()
+//{
 //	int num;
+//	printf("请输入一个数字");
 //	scanf("%d", &num);
-//	if (num = 1)
-//	{
-//		return 1;
-//	}
-//	else if (num == 0){
-//		return 0;
-//	}
-//	printf("用户选择有误，请重新选择");
-//
-//
+//	 er(num);
+//	 system("pause");
+//	return 0;
+
 //}
-//
-//char mine_map[ROW + 2][COL+2];
-//char show_map[ROW + 2][COL + 2];
-//void init()
-//{
-//	for (int row = 0; row < ROW + 2; row++)
-//	{
-//		for (int col = 0; col < COL + 2; col++)
-//		{
-//			char mine_map[ROW + 2][COL + 2] = '0';//0表示不是地雷，
-//			char show_map[ROW + 2][COL + 2] = '1';//1表示是地雷
-//		}
-//	}
-//}
-//void print()//先打印mine-map
-//{
-//	for (row; row < ROW + 2; row++)
-//	{
-//		printf("%d", char mine_map[ROW + 2][COL+2])
-//	}
-//	for (col; col < COL + 2; col++)
-//	{
-//		printf("%d", char mine_map[ROW + 2][COL+2])
-//	}
-//}
-//int not_mine_count = 0;
-////设置雷阵
-//meset(mine_map, '0', (ROW + 2)*(COL + 2));
-//meset(show_map), '*', (ROW + 2)*（COL + 2));
-//}
-//void displaymap(char map[ROW + 2][COL + 2])
-//{
-//}
-////提示用户输入坐标
-//while (1)
-//{
-//	int row = 0;
-//	int col = 0;
-//
-//	printf("请你输入一个坐标");
-//	scanf("%d%d", &row, &col);
-//
-//if (row<0 || row>ROW  || col<0 || col>COL)
-//{
-//	printf("你输入的不正确，请重新输入");
-//	continue;
-//}
-//}
-//if (mine_map[ROW ][COL ] = '1')
-//{
-//	printf("游戏结束\n");
-//	break;
-//}
-//    ++not_mine_count;
-//
-//if (not_mine_count==ROW*COL-mine_count){
-//	printf("扫雷结束\n");
-//	displaymap(mine_map);
-//	break;		
-//}
-// //如果扫雷没有结束，则要统计当前位置周围雷的个数
-//						
+//定义地图
+#define mine_count 10
+#define  ROW 10
+#define  COL 10
+char mine_map[ROW + 2][COL + 2];//地图初始化，有雷
+char show_map[ROW + 2][COL + 2];
+int menu(){
+	printf("********1开始游戏******* \n ");
+	printf("********0结束游戏*******\n");
+	printf("请输入你的选择");
+
+	int num;
+	while (1)
+	{
+
+		scanf("%d", &num);
+		if (num == 1)
+		{
+			return 1;
+		}
+		else if (num == 0){
+			return 0;
+		}
+		printf("用户选择有误，请重新选择");
+	}
+
+}
+void init()
+{
+	int row;
+	int col;
+	for ( row = 0; row < ROW + 2; row++)
+	{
+		for (col = 0; col < COL + 2; col++)
+		{
+			 mine_map[row][col] = '0';
+		//0表示不是地雷，
+		 show_map[row][col] = '0';
+		//*表示是地雷
+		}
+	}
+}
+void PrtMap(int n){//如果输出show n=1；如果输出mine n=0
+	int row;
+	int col;
+	//先打印第一行
+	for (col = 0; col <= COL; col++){
+		printf("%02d ", col);
+	}
+	printf("\n");
+	//在第一行下面答应横线
+	for (col = 0; col <= COL; col++){
+		printf("---");
+	}
+	printf("\n");
+	for (row = 1; row <= ROW; row++){
+		//每一行第一个为序号
+		printf("%02d| ", row);
+		for (col = 1; col <= COL; col++){
+			if (n == 1){
+				printf("%c  ", show_map[row][col]);
+			}
+			if (n == 0){
+				printf("%c  ", mine_map[row][col]);
+			}
+		}
+		printf("\n");
+	}
+}
+void PutMine(){
+	//埋雷
+	for (int count = 0; count < mine_count; count++){
+		int row = rand() % ROW + 1;
+		int col = rand() % COL + 1;
+		while (mine_map[row][col] == '*'){//如果重复
+			row = rand() % ROW + 1;
+			col = rand() % COL + 1;
+		}
+		mine_map[row][col] = '*';
+	}
+}
+int not_mine_count = 0;//计算不是雷的个数
+//如果扫雷没有结束，则要统计当前位置周围雷的个数
+void updateshowmap(int row, int col)
+{
+	int mine = 0;
+	//统计周围雷的数目
+	for (int rowl = row - 1; rowl < row + 2; rowl++){
+		for (int coll = col - 1; coll < col + 2; coll++){
+			if (rowl == row&&coll == col){
+				continue;
+			}
+			if (mine_map[rowl][coll] == '*'){
+				mine++;
+			}
+		}
+		show_map[row][col] = '0' + mine;
+	}
+}
+//提示用户输入坐标
+int playergame()
+{
+	while (1)
+	{
+		int row = 0;
+		int col = 0;
+
+		printf("请你输入一个坐标");
+		scanf("%d %d", &row, &col);
+
+		if (row<0 || row>ROW || col<0 || col>COL)
+		{
+			printf("你输入的不正确，请重新输入");
+			continue;
+		}
+
+		if (mine_map[row][col] == '*')
+		{
+			printf("游戏结束\n");
+			return 1;
+		}
+		else
+		{
+			updateshowmap(row, col);
+			++not_mine_count;
+		}
+
+		if (not_mine_count == ROW*COL - mine_count)
+		{
+			printf("扫雷结束\n");
+			printf("恭喜通关");
+			PrtMap(0);
+			return 1; 
+		}
+		return 0;
+	}
+}
+int main(){
+	//1.先打印菜单，判断是否进入游戏
+	if (menu()){
+		//2.打印show地图
+		PrtMap(1);
+		//3.布置雷阵
+		PutMine();
+		while (1){
+			PrtMap(1);
+		//4.让玩家做出选择
+		int result = playergame();
+		//5.判断游戏是否结束
+		if (result == 1){
+			break;
+		}
+		//7.如果没有回到第四步
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	srand(time(0));
+	if (menu()){
+		init();
+		PrtMap(1);
+		//埋雷
+		PutMine();
+		PrtMap(0);
+	}
+	system("pause");
+	return 0;
+}
