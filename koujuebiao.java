f0bc7bb2aeb74c08e99483aa19d3f2fe3df8bcaf
ytl1427698367 <1427698367@qq.com>
2018-11-03 23:16:02 +0800

    提交

---
+++ koujuebiao.java
@@ -0,0 +1,15 @@
+public class koujuebiao{
+	public static void main(String[]args){
+		int i,j;
+		for(i=1;i<10;i++){
+			for(j=1;j<=i;j++){
+			System.out.print(i+"*"+j+"="+i*j+"\t");}
+				//System.out.println(j);
+				//System.out.println("=");
+				//System.out.println(i*j);
+			
+				System.out.println();
+			
+		}
+	}
+}
