497e8d9e59d5ddbe49074c43c93e6a3108b3ad1f
ytl1427698367 <1427698367@qq.com>
2018-11-03 23:24:53 +0800

    提交

---
+++ process.java
@@ -0,0 +1,43 @@
+public class process{
+public static void main(String[]args) 
+throws IOException
+{
+//int a=10;
+	//byte b=(byte)a
+	//int numa=10;
+	//int numb=4;
+	//char a='A';
+	//num=a;
+	//System.out.println(numa/(double)numb);
+	//if (9=='9')
+	//{
+		
+
+		//System.out.println("xiangdeng");
+//	}
+	//else{
+	//	System.out.println("不相等");
+		//String srtName="java is good langrage";
+		//String strTion="bite is very good";
+	//System.out.println(srtName);
+	//System.out.println(strTion);
+	//System.out.println(srtName+strTion);
+	//}
+	//\t为制表符
+	//System.out.println("\"java 666\"");
+	//按照运算符的优先级进行运算
+	//int x=1;
+	//int v=2;
+	// int c=x++*v;
+	//逻辑运算符&&||(|)    !
+	System.out.println("请输入一个字符");
+	char c=(char)System.in.read();
+	switch(c){
+		case'a':{
+	
+		System.out.println("hello a");}
+	System.out.println(3<5||3>5);
+	
+	}
+}
+}
