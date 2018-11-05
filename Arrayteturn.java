23be5e2c49ccd3973d1289267e1bec57e50dcf8d
ytl1427698367 <1427698367@qq.com>
2018-11-05 22:57:42 +0800

    提交

---
+++ Arrayteturn.java
@@ -0,0 +1,27 @@
+
+//方法返回一个数组 
+public class Arrayteturn{
+	// public static int[] init(){
+		// return new int[] {2,5,5,6,5,4};
+	// }
+	// public static void Arrayprint(int[]b){
+		// for(int i = 0;i<b.length;i++){
+			// System.out.print(b[i]+"\t");
+		// }
+		
+	// }
+	// public static void main(String[] args){
+		// int[] b = init();
+	// Arrayprint(b);
+// }
+// }
+public static void main(String[] args){
+	int[]a = new int[]{52,54,52,56,58,69,54};
+	char []b = new char[]{'b','n','j','t','o','a','d','y'};
+	//java.util.Array.sort(a);
+	//java.util.Array.sort(b);
+	//printArray(a);
+	//printArray(b);
+	System.out.print(Arrays.toString(a));
+}
+}
