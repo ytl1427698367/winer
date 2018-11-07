b4c738701c56c52ae1fbcc1e7a6dd98127f86802
ytl1427698367 <1427698367@qq.com>
2018-11-07 21:13:16 +0800

    提交

---
+++ Sectet.java
@@ -0,0 +1,9 @@
+public class Sectet{
+	public Sectet(){
+		
+	}
+	public static void main(String[] args){
+		Grilfriend grilfriend = new Grilfriend("雪儿","45kg","164cm",23);
+		System.out.println(grilfriend.friend());
+	}
+}
