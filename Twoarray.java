d0f902234a5e8f783ffc9372d4a31d37ee85e363
ytl1427698367 <1427698367@qq.com>
2018-12-04 23:43:50 +0800

    提交

---
+++ Twoarray.java
@@ -0,0 +1,21 @@
+/**
+ * Author：TaiLong
+ * created:2018/12/3
+ */
+public class Twoarray {
+    public static void main(String[] args){
+       int two[][] =new int[4][4];
+       int i,j;
+       int k =0;
+       for(i=0;i<4;i++){
+           for(j=0;j<4;j++){
+               two[i][j]=k;
+               k++;
+
+System.out.print(k+" ");
+}
+System.out.println();
+}
+}
+}
+
