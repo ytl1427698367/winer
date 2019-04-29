8be2bf3df7d9360f1c2b6dccf362531b31fee85f
ytl1427698367 <1427698367@qq.com>
2019-04-29 23:10:00 +0800

    提交

---
+++ Addupto.java
@@ -0,0 +1,43 @@
+package Sort;
+
+/**
+ * Author：TaiLong
+ * created:2019/4/27
+ */
+//归并排序，也是分治算法
+public class Addupto {
+    public static void mergeSort(int[] array,int low,int high){
+        if(low==high-1){
+            return;
+        }
+        if(low>=high){
+            return;
+        }
+        int mid=low+(high-low)/2;//平均切开
+        mergeSort(array,low,mid);
+        mergeSort(array,mid,high);
+        merg(array,low,high,mid);
+    }
+    public static void merg(int[] array,int low,int high,int mid)
+    {
+        int[] extra=new int[high-low];
+        int i=low;
+        int j=mid;
+        int x=0;
+        while (i<=mid&&j<=high){
+            if(array[i]<=array[j]){
+                extra[x++]=array[i++];
+            }
+            else{
+                extra[x++]=array[j++];
+            }
+
+        }
+        while(i<mid){
+            extra[x++]=array[i++];
+        }
+        while(j<high){
+            extra[x++]=array[j++];
+        }
+    }
+}
