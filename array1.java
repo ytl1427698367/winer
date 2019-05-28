e44d31e18b45e96ef87b1041eb1da46621de53bb
ytl1427698367 <1427698367@qq.com>
2019-05-28 23:08:06 +0800

    提交

---
+++ array1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Author：TaiLong
+ * created:2019/5/28
+ */
+public class array1{
+    public static void main(String []args){
+        //Scanner sc=new Scanner(System.in);
+           // int b=sc.nextInt();
+           // int[] arr=new int[b];
+        int[] arr=new int[]{0,1,2,3,4,5,6,7};
+            for(int i=0;i<arr.length;i=i++){
+
+
+                }
+        }
+    }
+
