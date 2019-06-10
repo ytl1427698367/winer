0fc7ee264ac6ac877dcf241727d4cdd303cab4dc
ytl1427698367 <1427698367@qq.com>
2019-06-10 23:01:59 +0800

    歌很好听

---
+++ Split.java
@@ -0,0 +1,11 @@
+/**
+ * Author：TaiLong
+ * created:2019/5/29
+ */
+public class Split{
+    public static void main(String[] args){
+      String sr="w e r t y s d f g h d f g h";
+      String[] sr1=sr.split(" ");
+        System.out.println(sr1);
+    }
+}
