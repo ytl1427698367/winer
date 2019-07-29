8a4511f93091f099fc0c4b5199b87512656cd0b4
ytl1427698367 <1427698367@qq.com>
2019-07-29 10:13:48 +0800

    提交

---
+++ Stringadd.java
@@ -0,0 +1,10 @@
+public class Stringadd{
+    public static void main(String[]args){
+       Stringadd c=new Stringadd();
+        }
+    public String AddLonginteger(String addend,String augend){
+        String c;
+        c=addend+augend;
+        return null;
+    }
+}
