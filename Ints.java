fda937a32cf1cbc836e4a84bef0845388a39c893
ytl1427698367 <1427698367@qq.com>
2019-08-21 18:24:33 +0800

    统计表

---
+++ Ints.java
@@ -0,0 +1,19 @@
+package com.na;
+
+/**
+ * Author：TaiLong
+ * created:2019/8/20
+ */
+public class Ints {
+    public int myAtoi(String str) {
+        char[] b=str.toCharArray();
+        char[] c;
+        for(int i=0;i<b.length;i++){
+            if(b[i]==' '||(b[i]>='0'&b[i]<='9')){
+                int j=0;
+
+            }
+        }
+return 1;
+    }
+}
